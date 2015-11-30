0b32c09c076701b6bc6e3d1c5498e71e19710310
Davide Gessa <gessadavide@gmail.com>
2015-11-30 20:05:25 +0100

    quick install script update

--- quickinstall.sh
+++ quickinstall.sh
@@ -1,6 +1,5 @@
-CVMD_STABLE=8453aac1cbb02b48bbb2eb32e1b5b180cc436351
-LIBCVM_STABLE=1116c1e63c847a13ac6b4df6bb60b904e890e685
-KADPY_STABLE=b35ef5ecf6be0b65d83828271cfda3ead07a086c
+CVMD_STABLE=0043b16fad362299d027bb8f2b9c5b7eba1abcd0
+LIBCVM_STABLE=a9e6a755346bc9cf899766d04bfbd74389df9794
 
 PYTHON=python3
 PIP=pip3
@@ -11,17 +10,6 @@ sudo rm -rf ~/.contractvm
 
 #echo "Installing required packages..."
 #sudo apt-get install python3 python3-pip
-
-
-echo "Installing kad.py"
-sudo rm -rf ~/cvm-temp/
-mkdir -p ~/cvm-temp/
-cd ~/cvm-temp/
-git clone https://github.com/contractvm/kad.py.git
-cd kad.py
-git checkout $KADPY_STABLE
-sudo $PIP install -r requirements.txt
-sudo $PYTHON setup.py install
 
 
 echo "Installing contractvm"
