62a95985264d15833e8a0f5a7c8abc3b7d5f77d2
Davide Gessa <gessadavide@gmail.com>
2015-12-12 10:59:23 +0100

    chain reset

--- contractvmd/config.py
+++ contractvmd/config.py
@@ -43,7 +43,7 @@ def app_data_path (appauthor, appname, roaming=True):
 
 
 VERBOSE = 5
-APP_VERSION = '0.6.9.4'
+APP_VERSION = '0.6.9.5'
 APP_NAME = 'contractvm'
 APP_AUTHOR = 'Davide Gessa'
 DATA_DIR = app_data_path (appauthor=APP_AUTHOR, appname=APP_NAME)
@@ -59,7 +59,7 @@ CHAINS = {
 			'code': 'XTN',
 			'base_fee': 60000,
 			'genesis_block': "0000000033ab95c1231f6dede34aad172068250354beccfad156072c6d37e093",
-			'genesis_height': 625697,
+			'genesis_height': 625698,
 			'name': networks.full_network_name_for_netcode ('XTN'),
 			'seeds': [ ]
 		},
